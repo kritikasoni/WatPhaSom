00545fbaa2471ce06fe95a5327c7c386484c3d4f
kritikasoni <kritika_soni@cmu.ac.th>
2016-05-13 16:31:12 +0700

    install i18n

--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -16,6 +18,31 @@ namespace Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            // Change from the default of 'en'.
+            i18n.LocalizedApplication.Current.DefaultLanguage = "en";
+
+            // Change from the of temporary redirects during URL localization
+            i18n.LocalizedApplication.Current.PermanentRedirects = true;
+
+            // This line can be used to disable URL Localization.
+            //i18n.UrlLocalizer.UrlLocalizationScheme = i18n.UrlLocalizationScheme.Void;
+
+            // Change the URL localization scheme from Scheme1.
+            i18n.UrlLocalizer.UrlLocalizationScheme = i18n.UrlLocalizationScheme.Scheme2;
+
+            // Blacklist certain URLs from being 'localized' via a callback.
+            i18n.UrlLocalizer.IncomingUrlFilters +=
+                url => !url.LocalPath.EndsWith("sitemap.xml", StringComparison.OrdinalIgnoreCase);
+
+            // Blacklist certain URLs from being translated using a regex pattern. The default setting is:
+            i18n.LocalizedApplication.Current.UrlsToExcludeFromProcessing =
+                new Regex(@"(?:\.(?:less|css)(?:\?|$))|(?i:i18nSkip|glimpse|trace|elmah)");
+
+            // Whitelist content types to translate. The default setting is:
+            //i18n.LocalizedApplication.Current.ContentTypesToLocalize = new Regex(@"^(?:(?:(?:text|application)/(?:plain|html|xml|javascript|x-javascript|json|x-json))(?:\s*;.*)?)$");
+
+            // Change the types of async postback blocks that are localized
+            //i18n.LocalizedApplication.Current.AsyncPostbackTypesToTranslate = "updatePanel,scriptStartupBlock,pageTitle";
         }
     }
 }
