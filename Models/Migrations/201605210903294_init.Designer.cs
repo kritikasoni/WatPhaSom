ab02f2f269f997d0ead1537527f2cf23b116b4e6
kritikasoni <kritika_soni@cmu.ac.th>
2016-05-21 16:05:17 +0700

    Revert to last commit

--- Models/Migrations/201605210903294_init.Designer.cs
+++ Models/Migrations/201605210903294_init.Designer.cs
@@ -7,13 +7,13 @@ namespace Models.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class initial : IMigrationMetadata
+    public sealed partial class init : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(init));
         
         string IMigrationMetadata.Id
         {
-            get { return "201605201814023_initial"; }
+            get { return "201605210903294_init"; }
         }
         
         string IMigrationMetadata.Source
